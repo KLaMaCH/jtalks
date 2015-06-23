d417a072ec6556a9a5199c227144c21b61f4aab0
KLaMaCH <klamach89@gmail.com>
2015-06-23 16:48:55 +0300

    final version

--- src/test/java/JcommuneAT.java
+++ src/test/java/JcommuneAT.java
@@ -16,13 +16,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by kolchanov on 23.06.15.
- */
 public class JcommuneAT {
     private String[] registrationData = {"USERNAME10", "testtreesol+10@yandex.ru", "123"};
     private String[] mailLoginData = {"testtreesol@yandex.ru", "123456TS"};
     private String[] loginData = {"klamach89@gmail.com", "145278Goo"};
+    private String[] topicdata = {"NewTopic Subject", "This topic was created by auto-test"};
     private WebDriver driver;
 
     public static File makeSrceenshot(WebDriver driver, String pathName){
@@ -91,8 +89,9 @@ public class JcommuneAT {
     }
 
     @Test(priority = 1, alwaysRun = true)
-    public void NewTopic(){
+    public void NewTopic() throws Exception {
         driver.get("http://qa.jtalks.org/jcommune/login");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         WebElement username = driver.findElement(By.id("dialog-userName"));
         username.clear();
@@ -101,8 +100,30 @@ public class JcommuneAT {
         WebElement password = driver.findElement(By.id("dialog-password"));
         password.clear();
         password.sendKeys(loginData[1]);
+        password.submit();
 
+        driver.get("http://qa.jtalks.org/jcommune/branches/3");
+        makeSrceenshot(driver, "branch-page");
 
+        driver.findElement(By.xpath("//a[contains(@href,'/jcommune/topics/new')]")).click();
+        makeSrceenshot(driver, "newtopicpage");
+
+        WebElement subject = driver.findElement(By.id("subject"));
+        subject.clear();
+        subject.sendKeys(topicdata[0]);
+
+        if(!driver.findElement(By.id("sticked")).isSelected())
+            driver.findElement(By.id("sticked")).click();
+        if(!driver.findElement(By.id("announcement")).isSelected())
+            driver.findElement(By.id("announcement")).click();
+
+        WebElement messege = driver.findElement(By.id("postBody"));
+        messege.clear();
+        messege.sendKeys(topicdata[1]);
+        messege.submit();
+
+        Thread.sleep(1000);
+        makeSrceenshot(driver, "newtopic");
     }
 
     @AfterTest
